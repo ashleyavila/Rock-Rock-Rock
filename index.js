0782dc34da4c5d02cd062dcad1df7159c22b9822
Ashley Avila <ashley.avila@me.com>
2017-12-19 08:59:05 -0800

    jquery added

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-var form = document.getElementById('form');
+window.onload = function () {
+  $('#form').submit(function (event) {
+    event.preventDefault()
 
-form.onsubmit=function() {
-  //Question variables
-  var date = parseInt(document.querySelector('input[name = "date"]:checked').value);
-  var home = parseInt(document.querySelector('input[name = "home"]:checked').value);
-  var admire = parseInt(document.querySelector('input[name = "admire"]:checked').value);
-  var music = parseInt(document.querySelector('input[name = "music"]:checked').value);
-  var life = parseInt(document.querySelector('input[name = "life"]:checked').value);
+    // Logic to get the elements with the selectedOp elememnt
 
-  //Add values
-  var total = date + home + admire + music + life;
+    //Answer input
+    var answer; 
+    //document.getElementById('answer').innerHTML
 
-  //Answer input
-  var answer = document.getElementById('answer').textContent
+    //Determine which rock
+    if (total <= 5) {
+      answer = "You're a sedimentary rock";
+    } else if (total > 5 && total <= 10) {
+      answer = "You're a metamorphic rock";
+    } else if (total > 10 && total <= 15) {
+      answer = "You're an igneous rock";
+    }
 
-  if (total <= 5) {
-    answer = "You're a sedimentary rock";
-  } else if (total > 5 && total < 10) {
-    answer = "You're a metamorphic rock";
-  } else {
-    answer = "You're an igneous rock";
-  }
-  //Stop page from refreshing
-  return false;
+    var alert = document.createElement('div')
+    alert.className = 'alert alert-info'
+    alert.textContent = answer
+
+    var row = document.querySelector('form')
+    row.append(alert)
+  })
+
+  // Set the colors
+  $('.questionContainer').each(function (i, k) {
+    let selected = -1;
+    $(k).find('.optbox').each(function (io, ko) {
+      $(ko).click(function() {
+        if(selected !== ko) {
+          $(ko).addClass("selectedOpt");
+          $(ko).removeClass("opt");
+
+          if(selected !== -1) {
+            $(selected).addClass("opt");
+            $(selected).removeClass("selectedOpt");
+          }
+          
+          selected = ko;
+        }
+      });
+    });
+  });
+
+
+
+  $.each($('.questionContainer'), function(key, value) {
+    //console.log(key);
+    //console.log(value);
+    //console.log();
+  });
 }
