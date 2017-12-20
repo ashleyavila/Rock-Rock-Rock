dc0835ecf4daa6d901807865970c1f24438ff82d
Ashley Avila <ashley.avila@me.com>
2017-12-19 19:15:19 -0800

    alert works

--- index.js
+++ index.js
@@ -21,48 +21,61 @@ window.onload = function () {
   //   window.scrollTo(0, 2443);
   // });
 
+  var results = []
   
 
   $('#form').submit(function (event) {
     event.preventDefault()
 
-    // Logic to get the elements with the selectedOp elememnt
-
     //Answer input
-    var answer; 
-    //document.getElementById('answer').innerHTML
+    let answer = "";
+    let total = results.reduce(function (a,b) {
+      return a + b;
+    });
 
     //Determine which rock
     if (total <= 5) {
       answer = "You're a sedimentary rock";
     } else if (total > 5 && total <= 10) {
       answer = "You're a metamorphic rock";
-    } else if (total > 10 && total <= 15) {
+    } else if (total > 10 ) {
       answer = "You're an igneous rock";
     }
 
+    console.log(answer)
+
+    // Remove the previous answers
+    $('.alert').remove();
+    
+    // Create a new element and push it
     var alert = document.createElement('div')
     alert.className = 'alert alert-info'
     alert.textContent = answer
 
-    var row = document.querySelector('form')
-    row.append(alert)
+    $(alert).insertAfter('form');
   })
 
   // Set the colors
   $('.questionContainer').each(function (i, k) {
     let selected = -1;
+    results[i] = 0;
     $(k).find('.optbox').each(function (io, ko) {
       $(ko).click(function() {
+        // Update result
+        results[i] = io + 1;
+
+        // Update the colors selection
         if(selected !== ko) {
           $(ko).addClass("selectedOpt");
           $(ko).removeClass("opt");
 
+          // Restore background color of previous tile
           if(selected !== -1) {
             $(selected).addClass("opt");
             $(selected).removeClass("selectedOpt");
           }
           
+          // Update the selected tile to be the current tile
           selected = ko;
         }
       });
